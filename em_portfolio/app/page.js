9a2aff732f345941c1be1dca604d27fffc4ff7f1
Hayden Gorringe <33552561+HaydenGorr@users.noreply.github.com>
2024-10-21 09:27:43 +0100

    first push

---
+++ em_portfolio/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className=" bg-blue-smoke-50 w-screen text-blue-smoke-950 flex flex-col flex-grow">
+
+      <main className="">
+          <h1 className="font-baskerville">Emma's portfolio</h1>
+      </main>
+    </div>
+  );
+}
