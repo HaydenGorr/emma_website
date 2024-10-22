4d01261801c45a1f1f6db8c2a5d120a94c3f6514
Hayden Gorringe <33552561+HaydenGorr@users.noreply.github.com>
2024-10-22 23:14:06 +0100

    Update animated_bar.js

--- em_portfolio/app/components/animated_bar.js
+++ em_portfolio/app/components/animated_bar.js
@@ -6,7 +6,7 @@ export default function bar({ animate, colour, large_bars }) {
 
   const springs = useSpring({
     from: { y: -200, opacity: 0, height: large_bars ? '30rem' : '40rem' },
-    to: { y: large_bars ? 120 : 0, opacity: 1, height: large_bars ? '100rem' : '40rem' },
+    to: animate ? { y: large_bars ? 120 : 0, opacity: 1, height: large_bars ? '100rem' : '40rem' } : {},
     config: { tension: 30, friction: 5, mass: 1 },
   });
   
