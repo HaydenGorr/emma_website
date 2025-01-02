78d6f7c61b8e4c4d6a26de94e568e08f864e0d21
Hayden Gorringe <33552561+HaydenGorr@users.noreply.github.com>
2025-01-02 00:12:30 +0000

    Update page.tsx

--- em_portfolio/app/article/[documentId]/page.tsx
+++ em_portfolio/app/article/[documentId]/page.tsx
@@ -77,7 +77,7 @@ type PageProps = {
 
             <p className="mt-16 mb-2 px-4 py-2 rounded-lg bg-hibiscus-400 text-white font-semibold">{`Written by ${written_by}`}</p>
 
-            <div className="flex space-x-4 mt-4">
+            <div className="flex space-x-4 mt-4 mx-4">
               <p className="mb-8 px-4 py-2 rounded-lg bg-hibiscus-400 text-white font-semibold">{`published ${formatDateTime(createdOn)}`}</p>
               {beenUpdatedAfterPublish && <p className="mb-8 px-4 py-2 rounded-lg bg-hibiscus-400 text-white font-semibold">{`last updated ${formatDateTime(updatedAt)}`}</p>}
 
